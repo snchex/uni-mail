ba39f64a22d36238051bfa47d00a6dc6dfeec000
Javier Sánchez <javier.sanchezb084@gmail.com>
2022-09-06 19:14:40 -0400

    V 3.8

--- client/src/mails/pages/Home.js
+++ client/src/mails/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {  useState, useEffect } from 'react';
 import { useMails } from '../hooks';
 
  
@@ -10,22 +10,21 @@ export const Home = () => {
   const [wordEntered, setWordEntered] = useState("");
 
   const handleFilter = (event) => {
+
     const searchWord = event.target.value;
     setWordEntered(searchWord);
-    const newFilter = mails.filter((mail) => {
+     
+    const newFilter = mails.filter(mail => {
       return mail.user.toLowerCase().includes(searchWord.toLowerCase());
     });
+  
 
     if (searchWord === "") {
       setFilteredData([]);
     } else {
       setFilteredData(newFilter);
     }
-  };
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
   };
 
   useEffect(() => {
@@ -37,34 +36,70 @@ export const Home = () => {
     return () => clearTimeout(timer);
   });
 
+  const showError  = (filteredData.length === 0) ;
+  const showSearch = (filteredData.length !== 0);
+ 
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered("");
+  };
+
+
   return (
-    <div className="search">
-      <div className="searchInputs">
-        <input
-          type="text"
-          placeholder="Result mails"
-          value={wordEntered}
-          onChange={handleFilter}
-        />
-        <div className="searchIcon">
-          {filteredData.length === 0 ? (
-            <button>Buscar</button>
-          ) : (
-            <button id="clearBtn" onClick={clearInput} > RC</button>
-          )}
+    <div className="container mx-auto">
+      <div className="row">
+        <div className="col-5">
+          <h4>Busqueda</h4>
+          <hr />
+          <input
+            className='form-control'
+            type="text"
+            placeholder="Digite el correo"
+            value={wordEntered}
+            onChange={handleFilter}
+          />
+        
+            {filteredData.length === 0 ? (
+              <span></span>
+            ) : (
+              <button className='btn btn-warning' onClick={clearInput} >Limpiar</button>
+            )}
         </div>
+
+        <div className="col-7">
+          <h4>Resultado</h4>
+          <hr />
+          {filteredData.length !== 0 && (
+          <div className='table table-borderles'
+            style={{ display: showSearch ? '' : 'none' }}>
+            <tr className="border-bottom">
+              <td className='px-2'><b>Correo</b></td>
+              <td className='px-2'><b>Departamento</b></td>
+              <td className='px-2'><b>Tipo de Solicitud</b></td>
+              <td className='px-2'><b>Fecha de Inicio</b></td>
+            </tr>
+            {filteredData.slice(0, 15).map(mail => {
+              return (
+                
+                <tr className="border-bottom" key={mail.id}>
+                  <td >{mail.user} </td>
+                  <td>{mail.departamento}</td>
+                  <td>{mail.tipo}</td>
+                </tr>
+              );
+            })}
+          </div>
+        )}
+          <div className="alert alert-danger animate__animated animate__fadeIn" 
+                style={{ display: showError ? '' : 'none' }}>
+              No existe usuario <b>{wordEntered}</b>
+          </div>
+          
+
+
+        </div>
+      
       </div>
-      {filteredData.length !== 0 && (
-        <div className="dataResult">
-          {filteredData.slice(0, 15).map((mail, key) => {
-            return (
-              <p className="dataItem" target="_blank">
-                <p>{mail.user} </p>
-              </p>
-            );
-          })}
-        </div>
-      )}
     </div>
   );
 }
